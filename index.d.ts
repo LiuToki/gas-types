a7dde6e2116805a8e5082ad80b9e341213f5df1c
LiuToki <ray.usaneko@gmail.com>
2021-12-25 20:03:29 +0900

    [Parser] index にすべてをまとめた.

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,23 @@
 // "userSymbol": "Parser",
 // "libraryId": "1Mc8BthYthXx6CoIz90-JiSzSafVnT6U3t0z_W3hLTAX5ek4w0G_EIrNw",
 // "version": "8"
-/// <reference path="@types/Parser.d.ts"/>
+
+type ParserObject = {
+	from: (string, number) => ParserObject;
+	to: (string, number) => ParserObject;
+	offset: (number) => ParserObject;
+	setDirection: (string) => ParserObject;
+	setLog: () => ParserObject;
+	build: () => string;
+	iterate: () => string[];
+};
+
+declare namespace Parser {	
+	/*
+	* Extract parts from long content
+	*
+	* @param {String} content Text to parse
+	* @return {ParserObject} the result of the exponential calculation
+	*/
+	function data(content: string): ParserObject;
+}
